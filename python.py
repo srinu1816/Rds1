e8f5cef9f3f54372f9abf124ce930f8dc6537287
seswath srinivas <151889537+srinu1816@users.noreply.github.com>
2025-10-21 20:16:00 +0530

    Update python.py

--- python.py
+++ python.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 # Database configuration - UPDATED DATABASE NAME
 DB_CONFIG = {
-    'host': 'coupon-db.c5qcoy86mrln.eu-north-1.rds.amazonaws.com',
+    'host': 'coupon-db.cofom4a840i0.us-east-1.rds.amazonaws.com',
     'user': 'admin', 
     'password': 'CouponApp123!',
     'database': 'coupon_db',  # ← CHANGED TO coupon_db (with underscore)
